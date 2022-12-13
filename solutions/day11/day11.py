36e992d308d82e6fdd25e4139312266ec403684b
hani64 <haniasim@gmail.com>
2022-12-12 20:02:18 -0500

    day11 slight refactor

--- solutions/day11/day11.py
+++ solutions/day11/day11.py
@@ -12,12 +12,11 @@ for i in range(0,len(lines),7):
   monkey_ops.append(lines[i+2].split('=')[-1].lstrip())
   monkey_tests.append([int(re.findall(r'\d+', lines[i+3])[0]), int(re.findall(r'\d+', lines[i+4])[0]), int(re.findall(r'\d+', lines[i+5])[0])])
 
-
-monkey_inspects = [0 for i in range(len(monkey_items))]
+monkey_inspects = [0 for _ in range(len(monkey_items))]
 lcm = 1
 for l in monkey_tests:
   lcm *= l[0]
-for r in range(20 if p1 else 10000):
+for _ in range(20 if p1 else 10000):
   for m in range(len(monkey_items)):
     while monkey_items[m]:
       monkey_inspects[m] += 1
@@ -26,8 +25,7 @@ for r in range(20 if p1 else 10000):
       if p1:
         item = item // 3
       # Part 2
-      else:
-        item = item - (item // lcm) * lcm
+      item = item % lcm
       if item % monkey_tests[m][0] == 0:
         monkey_items[monkey_tests[m][1]].append(item)
       else:
